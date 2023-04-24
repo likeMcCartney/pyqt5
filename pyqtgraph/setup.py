1c1ce16e54f299efb2c26e46b3d6c48934680737
Pavel Kanashkov <pavel.kanashkov@advalange.com>
2023-04-24 11:30:20 +0300

    test graphs

--- pyqtgraph/setup.py
+++ pyqtgraph/setup.py
@@ -6,11 +6,33 @@ import sys
 from gui.mainwindow import Ui_MainWindow
 
 
+listX = [0,100]
+listY1 = [0,0]
+listY2 = [1,1]
+
+for i in range(2, 1000):
+    if listX[i-1] == listX[i-2]:
+        listX.append(listX[i-1] + 100)
+        listY1.append(listY1[i-1])
+        listY2.append(listY2[i-1])
+    else:
+        listX.append(listX[i-1])
+        listY1.append(0) if listY1[i-1] == 1 else listY1.append(1)
+        listY2.append(0) if listY2[i-1] == 1 else listY2.append(1)
+        
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs) -> None:
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.plot(listX, listY1)
+        self.plot(listX, listY2, 2)
+
+    def plot(self, x, y, yoffs=0):
+        for i in range(0, len(y)):
+            y[i] += yoffs
+        self.pyqtgraphWidget.plot(x, y)
 
 
 if __name__ == "__main__":
